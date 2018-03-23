ca14ba58cb33fecf70c6cd1daf0161c5c8b3ff82
Dylan Harbaugh <dpharbau@uncg.edu>
2018-03-23 15:30:42 -0400

    Solution with some test data
    Added soln with test data

---
+++ Potato/potato.py
@@ -0,0 +1,77 @@
+variables = raw_input()
+string = raw_input()
+
+variables = variables.split(" ")
+mode = variables[0]
+
+# 5 variables for the addition/division and when to do that
+a = int(variables[1])
+b = int(variables[2])
+c = int(variables[3])
+d = int(variables[4])
+e = int(variables[5])
+
+# Shift value starts equal to the length of the string
+m = len(string)
+
+string = list(string)
+
+encrypted = ""
+
+decrypted = ""
+
+offsetarray = []
+
+processed = 0
+# Don't want the mod check to run when processed is 0
+first = True
+# generate the array for character offsets, need for both encryption and decryption
+for character in string:
+    if (processed % b) == 0 and not first:
+        a += c
+    if (processed % d) == 0 and not first:
+        a /= e
+    if ord(character) != 32:
+        offsetarray.append(m)
+        print m
+    else:
+        offsetarray.append(0)
+        print m
+    m += a
+    processed += 1
+    first = False
+
+count = 0
+
+# If we're decrypting sub our offsets from the letters and make sure they're in bounds
+if mode == "d":
+
+    for character in list(string):
+        number = ord(character)
+        if number is not 32:  # Don't operate on spaces (ASCII value 32)
+            number -= offsetarray[count]
+            # Check that we aren't out of bounds
+            while number >= 123:  # ASCII value of 1+z
+                number -= 26
+            while number <= 96:  # ASCII value of 1-a
+                number += 26
+        decrypted += chr(number)
+        count += 1
+    print decrypted
+# If we aren't decrypting, we must be encrypting, so just add instead of subtract
+else:
+    print "Starting encryption"
+    for character in list(string):
+        number = ord(character)
+        if number is not 32:
+            number += offsetarray[count]
+            while number >= 123:
+                number %= 122
+                if number <97:
+                    number += 96
+            while number <= 96:
+                number += 26
+        encrypted += chr(number)
+        print encrypted
+        count += 1
+    print encrypted
